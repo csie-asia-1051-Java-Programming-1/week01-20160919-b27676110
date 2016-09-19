9e620cd1fdc47bc0b57aeae5272d8ae180bd23e2
b27676110 <105021036@live.asia.edu.tw>
2016-09-19 16:12:05 +0800

    week01_20160919

--- week01_20160919/src/ex/ex04_105021036.java
+++ week01_20160919/src/ex/ex04_105021036.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 /*
  * Topic: 請寫一個程式讓使用者輸入數字 a，程式 計算 a 平方根並輸出 (請使用 Math.sqrt(a) 工具)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021036 鄭東欣
  */
 
-public class ex04_1050210XX {
+public class ex04_105021036 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
-        int b = scn.nextInt();
-        System.out.println(Math.pow(a,b));
+        System.out.println(Math.sqrt(a));
 	}
 
 }
